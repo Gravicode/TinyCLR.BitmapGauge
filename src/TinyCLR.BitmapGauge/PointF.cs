e727232ca183d4360c64aec2e2f1294b94ec7f4a
mifmasterz <mifmasterz@yahoo.com>
2022-02-21 08:25:39 +0700

    init

---
+++ src/TinyCLR.BitmapGauge/PointF.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Threading;
+
+namespace TinyCLR.BitmapGauge
+{
+    public class PointF
+    {
+        public PointF(float ax, float ay)
+        {
+            this.X = ax;
+            this.Y = ay;
+
+        }
+        public float X { get; set; }
+        public float Y { get; set; }
+    }
+}
