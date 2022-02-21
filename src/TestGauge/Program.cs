c997cd159e31029a4fdb26798a172095ec1726a8
mifmasterz <mifmasterz@yahoo.com>
2022-02-21 10:32:29 +0700

    add desktop version for comparation

---
+++ src/TestGauge/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var gauge = new BitmapGauge.BitmapGauge(500,500);
+gauge.Value = 70;
+gauge.ThresholdPercent = 50;
+var bmp = gauge.GetGauge();
+bmp.Save(Directory.GetCurrentDirectory() + "\\gauge.jpg");
